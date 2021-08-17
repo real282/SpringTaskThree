e99a7caa07da863e1b006e1c495b70962263993b
Evgeny Roznin <real282@mail.ru>
2021-08-17 09:02:44 +0800

    Исправлен кролик

--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -1,13 +1,12 @@
 package koschei.models;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-@Component
 public class Rabbit4 {
 
-    @Autowired
     private Duck5 duck;
+
+    public Rabbit4(Duck5 duck) {
+        this.duck = duck;
+    }
 
     @Override
     public String toString() {
